8cd09070c76595a3bd69f54981e3fc5df6d35326
Pablo Codelia <pecodelia@gmail.com>
2023-09-06 23:27:57 -0300

    Delete venv directory

--- venv/lib/python3.10/site-packages/djangocms_blog/taggit_urls.py
+++
@@ -1,7 +0,0 @@
-# This file is used on divio cloud only during automatic setup
-from django.urls import path  # pragma: no cover
-from django.urls import include
-
-urlpatterns = [  # pragma: no cover
-    path("taggit_autosuggest/", include("taggit_autosuggest.urls")),
-]
--- venv/lib/python3.10/site-packages/taggit_autosuggest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-
-from .views import list_tags
-
-
-urlpatterns = [
-    re_path(r"^list/$", list_tags, name="taggit_autosuggest-list"),
-    re_path(r"^list/(?P<tagmodel>[\._\w]+)/$", list_tags, name="taggit_autosuggest-list"),
-]
